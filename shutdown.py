9627665ec5ed8c4ab3aab410309ddcdf0abdffa5
kamal-devtron <128121299+kamal-devtron@users.noreply.github.com>
2024-01-21 22:22:47 +0530

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -3,13 +3,13 @@ import subprocess as sp , sys, os
 username=sys.argv[1]
 password=sys.argv[2]
 tenantid=sys.argv[3]
-print("Starting Automation cluster")
+print("Stopping Automation cluster")
 def auth():
     authoutput=sp.getstatusoutput("az login --service-principal -u {} -p {} --tenant {}".format(username,password,tenantid))
     return authoutput
 
 def stop():
-     str=sp.getoutput("az aks start --resource-group automation-rg --name automation-aks   --output json")
+     str=sp.getoutput("az aks stop --resource-group automation-rg --name automation-aks   --output json")
      return str
 
 
@@ -20,6 +20,6 @@ if(authout[0]==0):
     res=stop()
     count=sp.getoutput("az aks nodepool list --resource-group automation-rg  --cluster-name automation-aks  --query \"[?mode=='System'].count\" -o tsv") 
     print(count)
-    print("Started sucessfully")
+    print("Stopped sucessfully")
 else:
     print("Authentication failed",authout[1])
